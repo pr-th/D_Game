a903fdf65eb3ac73363a7ab15df30acd8f436534
pr-th <prthmhll@gmail.com>
2025-09-10 07:48:58 +0530

    Created the info tab

---
+++ info.gd
@@ -0,0 +1,68 @@
+extends Node2D
+@export var json_file: String = "res://Assets/pages.json"
+var pages: Array = []
+var current_page: int = 0
+
+@onready var left_page = $Left_page
+@onready var right_page = $Right_page
+@onready var next_button = $next_button
+@onready var prev_button = $prev_button
+@onready var home_button = $home_btn
+func _ready():
+	load_pages()
+	update_book()
+	next_button.modulate = Color(1, 1, 1, 0)  
+	prev_button.modulate = Color(1, 1, 1, 0)
+	home_button.modulate = Color(1,1,1,0)
+	home_button.pressed.connect(_on_home)
+	next_button.pressed.connect(_on_next_page)
+	prev_button.pressed.connect(_on_prev_page)
+	
+func load_pages():
+	var file = FileAccess.open(json_file, FileAccess.READ)
+	if file:
+		var result = JSON.parse_string(file.get_as_text())
+		if typeof(result) == TYPE_ARRAY:
+			pages = result
+	file.close()
+
+func update_book():
+	if current_page < pages.size():
+		var left = pages[current_page]
+		left_page.clear()
+		left_page.push_font_size(28)
+		left_page.push_paragraph(HORIZONTAL_ALIGNMENT_CENTER)
+		left_page.add_text(left.get("title", ""))
+		left_page.pop()
+		left_page.add_text("\n\n")
+		left_page.push_font_size(15)
+		left_page.add_text(left.get("content", ""))
+	else:
+		left_page.text = ""
+	
+	if current_page + 1 < pages.size():
+		var right = pages[current_page + 1]
+		right_page.clear()
+		right_page.push_font_size(28)
+		right_page.push_paragraph(HORIZONTAL_ALIGNMENT_CENTER)
+		right_page.add_text(right.get("title", ""))
+		right_page.pop()
+		right_page.add_text("\n\n")
+		right_page.push_font_size(15)
+		right_page.add_text(right.get("content", ""))
+	else:
+		right_page.text = ""
+
+
+func _on_next_page():
+	if current_page + 2 < pages.size():
+		current_page += 2
+		update_book()
+
+func _on_prev_page():
+	if current_page - 2 >= 0:
+		current_page -= 2
+		update_book()
+
+func _on_home():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
--- main_menu.gd
+++ main_menu.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+@onready var start_btn = $Start_BTN
+@onready var info_btn = $Info_BTN
+@onready var question_btn = $Question_BTN
+@onready var crown_btn = $Crown_BTN
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	start_btn.modulate = Color(1, 1, 1, 0)
+	info_btn.modulate = Color(1, 1, 1, 0)
+	question_btn.modulate = Color(1, 1, 1, 0)
+	crown_btn.modulate = Color(1, 1, 1, 0)
+	
+func _on_info_btn_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://Scenes/info.tscn")
+
+
+func _on_crown_btn_pressed() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_question_btn_pressed() -> void:
 	pass
+
+
+func _on_start_btn_pressed() -> void:
+	pass # Replace with function body.
