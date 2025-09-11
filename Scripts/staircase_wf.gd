fba0248978f60a2263743f6df73fc7f5f51511ba
Yash Dharurkar <yash.dharurkar@mitwpu.edu.in>
2025-09-12 01:35:25 +0530

    final wala final

--- Scripts/staircase_wf.gd
+++ Scripts/staircase_wf.gd
@@ -15,3 +15,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		# Change to the hallway scene
 		get_tree().change_scene_to_file("res://Scenes/hallway.tscn")
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		# Change to the done scene
+		get_tree().change_scene_to_file("res://Scenes/done.tscn")
