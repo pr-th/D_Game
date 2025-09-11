b65d305c8f488b36d1416e238ee767a0ed5d088b
CoderGama <ga.ma.0.1208@gmail.com>
2025-09-12 03:00:02 +0530

    fire

--- Scripts/staircase.gd
+++ Scripts/staircase.gd
@@ -21,3 +21,9 @@ func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		# Change to the fire extinguisher scene
 		get_tree().change_scene_to_file("res://Scenes/fire_extinguisher.tscn")
+
+func _on_area_2d_3_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		# Change to the fire extinguisher scene
+		global.lose_message="[center][color=#FF8C00]Who thought touching fire was a good idea?[/color][/center]"
+		get_tree().change_scene_to_file("res://Scenes/lose_screen.tscn")
